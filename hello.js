bfc1d8e7903505618dfb715a454caa11bdbad696
YOSAWY <y.redda@gmail.com>
2023-06-13 01:57:19 +0000

    Hello First commit

--- hello.js
+++ hello.js
@@ -1,4 +1,5 @@
-const sayHello = function(name){
-  console.log("Hello, world " + name);
+const returnSayHello = function(name){
+  return "Hello" + name;
 }
-sayHello("Caliban");
+const greeting = returnSayHello('John');
+console.log(greeting);
